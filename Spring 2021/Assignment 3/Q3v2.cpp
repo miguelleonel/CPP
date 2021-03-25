c268145e31b36f18405a75144e4737361dbd4493
Miguel <59388485+miguelleonel@users.noreply.github.com>
2021-03-25 12:26:30 -0500

    Update Q3v2.cpp
    Fixed indention formatting that changed when uploaded to github.

--- Spring 2021/Assignment 3/Q3v2.cpp
+++ Spring 2021/Assignment 3/Q3v2.cpp
@@ -17,73 +17,73 @@ class Car
 	    Car(string make, string model) 
     	{
     		this->make = make;
-            this->model = model;
+		this->model = model;
     		speed = 0;
     		gear = 1;
     		lane = "right";
     	}
         void gearUp() 
-		{
+	{
             gear++;
             if(gear > 6) 
-			{
-				cout<<"*You're in the highest gear already!*"<<endl;
+		{
+		cout<<"*You're in the highest gear already!*"<<endl;
                 gear = 6;
-            }
+    		}
         }
         void gearDown() 
+	{
+		gear--;
+		if(gear < 1) 
 		{
-            gear--;
-            if(gear < 1) 
-			{
-				cout<<"*You're already in first gear!*"<<endl;
-                gear = 1;
-            }
-        }
-		void changeLane(char c)
-		{
-			switch(c)
-			{
-				case 'l':
-					if(lane=="left")
-					{
-						cout<<"*Already in left-most lane!*"<<endl;
-						break;
-					}
-					else if(lane=="middle")
-					{
-						cout<<"*Switched to left lane*"<<endl;
-						lane="left";
-						break;
-					}
-					else if(lane=="right")
-					{
-						cout<<"*Switched to middle lane*"<<endl;
-						lane="middle";
-						break;
-					}
-				case 'r':
-					if(lane=="right")
-					{
-						cout<<"*Already in right-most lane!*"<<endl;
-						break;
-					}
-					else if(lane=="middle")
-					{
-						cout<<"*Switched to right lane*"<<endl;
-						lane="right";
-						break;
-					}
-					else if(lane=="left")
-					{
-						cout<<"*Switched to middle lane*"<<endl;
-						lane="middle";
-						break;
-					}
-			}
+			cout<<"*You're already in first gear!*"<<endl;
+			gear = 1;
 		}
-        void accelerate() 
+        }
+	void changeLane(char c)
+	{
+		switch(c)
 		{
+			case 'l':
+				if(lane=="left")
+				{
+					cout<<"*Already in left-most lane!*"<<endl;
+					break;
+				}
+				else if(lane=="middle")
+				{
+					cout<<"*Switched to left lane*"<<endl;
+					lane="left";
+					break;
+				}
+				else if(lane=="right")
+				{
+					cout<<"*Switched to middle lane*"<<endl;
+					lane="middle";
+					break;
+				}
+			case 'r':
+				if(lane=="right")
+				{
+					cout<<"*Already in right-most lane!*"<<endl;
+					break;
+				}
+				else if(lane=="middle")
+				{
+					cout<<"*Switched to right lane*"<<endl;
+					lane="right";
+					break;
+				}
+				else if(lane=="left")
+				{
+					cout<<"*Switched to middle lane*"<<endl;
+					lane="middle";
+					break;
+				}
+		}
+	}
+        void accelerate() 
+	{
             speed += 5;
             if (speed>85)
             {
@@ -92,22 +92,22 @@ class Car
             }
         }
         void deaccelerate() 
+	{
+		speed -= 5;
+		if(speed < 0) 
 		{
-            speed -= 5;
-            if(speed < 0) 
-			{
-			    cout<<"*You're already at 0 MPH.*"<<endl;
-                speed = 0;
-            }
+			cout<<"*You're already at 0 MPH.*"<<endl;
+			speed = 0;
+            	}
         }
-		void displayStatus()
-		{
-			cout<<"\nMake: "<< make<<endl;
-			cout<<"Model: "<< model<<endl;
-			cout<<"Gear: "<< gear<<endl;
-			cout<<"Lane: "<< lane<<endl;
-			cout<<"Speed: "<< speed<<" MPH\n";
-		}
+	void displayStatus()
+	{
+		cout<<"\nMake: "<< make<<endl;
+		cout<<"Model: "<< model<<endl;
+		cout<<"Gear: "<< gear<<endl;
+		cout<<"Lane: "<< lane<<endl;
+		cout<<"Speed: "<< speed<<" MPH\n";
+	}
 };
 
 void actionMenu()
@@ -130,25 +130,26 @@ void carMenu()
 	cout<<"1. Ford Mustang"<<endl;
 	cout<<"2. Chevrolet Camaro"<<endl;
 	cout<<"3. Dodge Charger"<<endl;
-	cout<<"\nq. Quit program."<<endl;   // technically true, but any letter works here...
+	cout<<"\nq. Quit program."<<endl;
 	cout<<"Your choice: ";	
 }
 
 
 int main ()
 {
-    char c,n;   // action choice & car choice
-                // easier to check for valid input when used as char versus int
+	char c,n;   // action choice & car choice
+		// easier to check for valid input when used as char versus int
 
 	Car mycar("Ford","Mustang");
 	Car my2car("Chevrolet","Camaro");
 	Car my3car("Dodge","Charger");
 	cout<<"Enter q at any time to quit."<<endl;
-    VEHICLE:carMenu();cin >> n;
+	
+	VEHICLE:carMenu();cin >> n;
 	switch (n)
 	{
 	    case 'q':
-	        break;
+		break;
 		case '1':         // Mustang
 			LOOP1:mycar.displayStatus();actionMenu();cin >> c;
 			switch(c)
@@ -174,10 +175,10 @@ int main ()
 				case 'm':
 					goto VEHICLE;  
 				case 'q':   // using this case instead of while(c!='q')
-				    break;
+					break;
 				default:
-                    cout<<"Invalid key"<<endl;
-                    goto LOOP1;  
+					cout<<"Invalid key"<<endl;
+					goto LOOP1;  
 			}
 			break;
 		case '2':         // Camaro
@@ -205,10 +206,10 @@ int main ()
 				case 'm':
 					goto VEHICLE;  
 				case 'q':   // using this case instead of while(c!='q')
-				    break;
+					break;
 				default:
-                    cout<<"Invalid key"<<endl;
-                    goto LOOP2;  
+				    	cout<<"Invalid key"<<endl;
+				    	goto LOOP2;  
 			}
 			break;
 		case '3':         // Charger
@@ -236,10 +237,10 @@ int main ()
 				case 'm':
 					goto VEHICLE;  
 				case 'q':   // using this case instead of while(c!='q')
-			        break;
+					break;
 				default:
-                    cout<<"Invalid key"<<endl;
-                    goto LOOP3;  
+			    		cout<<"Invalid key"<<endl;
+			    		goto LOOP3;  
 			}
 			break;
 		default:
